5bb1bd4856447473877d1c3a4f9ee5b374a8c65a
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-03-13 08:51:07 -0500

    feat: Add Tailwind CSS to the Repository

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './docusaurus.config.js',
+    './docs/**/*.{md,mdx}',
+    './blog/**/*.{md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
